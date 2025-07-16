fb77c290f55e216e2c85c005a2bcfff58be20dae
Dada6x <dada.66.6x.outlook@gmail.com>
2025-07-16 05:23:56 +0300

    idk

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+// next.config.ts
+const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
+  basePath: '/dada6x.github.io',
+  assetPrefix: '/dada6x.github.io/',
 };
 
 export default nextConfig;
